08592b8afb362354548cd4f011c0874e099d4302
worldvanquisher <lowx@eduroam-campus-10-36-65-163.campus-dynamic.uwaterloo.ca>
2026-03-07 14:43:43 -0500

    add json streamline

--- pipeline.py
+++ pipeline.py
@@ -3,6 +3,8 @@ import json
 
 ser = serial.Serial("/dev/cu.usbmodem101",9600)
 
+outfile = open("sensor_stream.jsonl","a")
+
 while True:
 
     line = ser.readline().decode(errors="ignore").strip()
@@ -12,7 +14,13 @@ while True:
 
     try:
         data = json.loads(line)
+
+        data["timestamp"] = time.time()
+
         print("data:", data)
 
-    except Exception:
+        outfile.write(json.dumps(data) + "\n")
+        outfile.flush()
+
+    except:
         print("invalid data:", line)
